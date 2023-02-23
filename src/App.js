a26147d001e95e72eb0b6ac0510368bd59fc886b
khaled nashwan <khaled97nash@gmail.com>
2023-02-23 13:00:24 +0200

    add structure to my app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./screen/About";
+import HomeScreen from "./screen/HomeScreen";
+import NotFound from "./screen/NotFound";
 
 function App() {
   return (
     <>
-      <div className=""></div>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
